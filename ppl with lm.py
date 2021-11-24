29f1d01d28d8dcd4de48ef42079ab6d99f8474a2
wangyuchi369 <wangyuchi369@163.com>
2021-11-24 17:52:31 +0800

    compute ppl

--- ppl with lm.py
+++ ppl with lm.py
@@ -1,49 +1,46 @@
 from transformers import GPT2LMHeadModel, GPT2TokenizerFast
-from datasets import load_dataset
+import json
 import torch
 from tqdm import tqdm
-device = 'cuda'
+import timeit
+#%%
 model_id = 'gpt2'
-model = GPT2LMHeadModel.from_pretrained(model_id).to(device)
+model = GPT2LMHeadModel.from_pretrained(model_id).to('cuda')
 tokenizer = GPT2TokenizerFast.from_pretrained(model_id)
+#%%
+def ppl(answer, statement):
+    sentence = statement.replace('**blank**', answer)
+    encodings = tokenizer(sentence, return_tensors='pt')
+    input_ids = encodings.input_ids.to('cuda')
+    with torch.no_grad():
+        outputs = model(input_ids, labels=input_ids)
+        cross_entropy = outputs[0]
+    ppl = torch.exp(cross_entropy)
+    return ppl
+#%%
+answer_file = 'a.json'
+statement_file = 'b.json'
+with open(answer_file,'r') as f:
+    answer_list = json.load(f).values()
+with open(statement_file, 'r') as f:
+    statement_struct = json.load(f)
+    questions_list = statement_struct['questions']
+start=timeit.default_timer()
+for question in tqdm(questions_list):
+    question.pop('vilt_top100_answers')
+    statement = question['statement']
+    res = dict()
+    for each_answer in answer_list:
+        res[each_answer] = ppl(each_answer,statement)
+    sort = sorted(res.items(),key=lambda x:x[1])[0:100]
+    question['answer'] = [answer[0] for answer in sort ]
+with open('c.json','w') as f:
+    json.dump(statement_struct, f, indent =2)
+end=timeit.default_timer()
+print('Running time: %s Seconds'%(end-start))
 
 #%%
-encodings = tokenizer('hello with project he is remote refused', return_tensors='pt')
-max_length = model.config.n_positions
-stride = 512
+ppl('down','he is looking **blank**')
 
-# nlls = []
-# for i in tqdm(range(0, encodings.input_ids.size(1), stride)):
-#     begin_loc = max(i + stride - max_length, 0)
-#     end_loc = min(i + stride, encodings.input_ids.size(1))
-#     trg_len = end_loc - i    # may be different from stride on last loop
-#     input_ids = encodings.input_ids[:,begin_loc:end_loc].to(device)
-#     target_ids = input_ids.clone()
-#     target_ids[:,:-trg_len] = -100
-#
-#     with torch.no_grad():
-#         outputs = model(input_ids, labels=target_ids)
-#         neg_log_likelihood = outputs[0] * trg_len
-#
-#     nlls.append(neg_log_likelihood)
 
-nlls = []
-for i in range(0, encodings.input_ids.size(1)):
-    begin_loc = 0
-    end_loc = i+2
-    trg_len = 1   # may be different from stride on last loop
-    input_ids = encodings.input_ids[:,begin_loc:end_loc].to(device)
-    target_ids = input_ids.clone()
-    if i==0:
-        target_ids[:,:] = input_ids[:,:]
-    else:
-        target_ids[:,:-trg_len] = -100
 
-    with torch.no_grad():
-        outputs = model(input_ids, labels=target_ids)
-        neg_log_likelihood = outputs[0] * trg_len
-
-    nlls.append(neg_log_likelihood)
-
-ppl = torch.exp(torch.stack(nlls).sum() / end_loc)
-print(ppl)
